4293fd340e74f714bf2960f26b02e2597068abdd
Alvaro Koke <rekoke92@gmail.com>
2025-04-30 18:57:45 +0200

    Fix: delete duplicated import

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { PostHogProvider } from '@/components/analytics/PostHogProvider';
 import { routing } from '@/libs/i18nNavigation';
-import { NextIntlClientProvider, NextIntlClientProvider } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, setRequestLocale } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import '@/styles/global.css';
